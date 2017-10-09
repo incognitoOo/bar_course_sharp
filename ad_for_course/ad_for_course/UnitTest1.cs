eba1718293164f05b724c5617b34f361bcdcf109
Ринат Ри <wwinssett@gmail.com>
2017-10-09 21:20:14 +0300

    Commit 1 (empty project)

---
+++ ad_for_course/ad_for_course/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ad_for_course
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
