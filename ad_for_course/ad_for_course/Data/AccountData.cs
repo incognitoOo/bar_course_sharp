e6b47669b05c17a9c9582d53ce07c227e93d0b8c
Ринат Ри <wwinssett@gmail.com>
2017-10-18 20:05:17 +0300

    Commit 4 (HW4)

--- ad_for_course/ad_for_course/Data/AccountData.cs
+++ ad_for_course/ad_for_course/Data/AccountData.cs
@@ -10,7 +10,7 @@ namespace WebAddressbookTests
 
     {
         private string username;
-        public string userpassword;
+        private string userpassword;
         public AccountData(string username,string userpassword) { this.username = username; this.userpassword = userpassword; }
         public string Username { get { return username; } set { username = value; } }
         public string Userpassword { get { return userpassword; } set { userpassword = value; } }
